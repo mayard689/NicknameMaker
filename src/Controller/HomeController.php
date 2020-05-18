66e225f5de5999b8883bba0ca509de334875d085
mayard689 <doc-albert@laposte.net>
2020-05-18 15:16:46 +0200

    now a route exists to make 3 random nicknames 10 chars long

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -25,4 +25,40 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', ['results'=>$results]);
 
     }
+
+    /**
+     * @return string
+     *
+     * @Route("makeName", name="makeName")
+     */
+    public function makeName()
+    {
+        $results=$this->generateNickName(3, 10);
+        return $this->render('home/index.html.twig', ['results'=>$results]);
+
+    }
+
+    /**
+     * @param int $number : number of nickname to make
+     * @return array
+     */
+    private function generateNickName(int $number, int $length) : array
+    {
+        $nicknames=[];
+
+        $availableCharacters='azertyuiopmlkjhgfdsqwxcvbn';
+        $availableCharactersNumbers=strlen($availableCharacters);
+
+        for ($i=0;$i<$number;$i++) {
+            $nickname="";
+
+            for ($j=0;$j<$length;$j++) {
+                $nickname .= $availableCharacters[rand(0, $availableCharactersNumbers - 1)];
+            }
+
+            $nicknames[]=$nickname;
+        }
+
+        return $nicknames;
+    }
 }
